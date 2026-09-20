bb07ec0ff7ee2f9052779dc21ed168782d45cd33
Keith skaggs <240277128+ksksrbiz-arch@users.noreply.github.com>
2026-09-20 12:54:35 -0700

    fix: remove duplicated map world wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -341,7 +341,6 @@ export default function App(){
        <filter id="landShadow" x="-20%" y="-20%" width="140%" height="140%"><feDropShadow dx="0" dy="5" stdDeviation="5" floodColor="#26383a" floodOpacity=".28"/></filter>
        <filter id="paperNoise"><feTurbulence type="fractalNoise" baseFrequency=".65" numOctaves="2" stitchTiles="stitch" result="noise"/><feColorMatrix in="noise" type="saturate" values="0" result="gray"/><feComponentTransfer><feFuncA type="table" tableValues="0 .055"/></feComponentTransfer><feBlend in="SourceGraphic" in2="gray" mode="multiply"/></filter>
       </defs>
-      <g ref={mapWorldRef} className="mapWorld">
       <MapBackground/>
       <g ref={mapWorldRef} className="mapWorld">
       <MapGeography/>
